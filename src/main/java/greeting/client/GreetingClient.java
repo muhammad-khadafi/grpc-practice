c9923de9bbcd7bb03bbb1c2bb998e955b267460f
muhammad.khadafi <yusuf.khadafi@pusilkom.com>
2025-04-25 11:20:58 +0700

    create unary service

--- src/main/java/greeting/client/GreetingClient.java
+++ src/main/java/greeting/client/GreetingClient.java
@@ -1,5 +1,8 @@
 package greeting.client;
 
+import com.proto.greeting.GreetingRequest;
+import com.proto.greeting.GreetingResponse;
+import com.proto.greeting.GreetingServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
@@ -7,17 +10,32 @@ public class GreetingClient {
 
     public static void main(String[] args) {
 
+        if (args.length == 0) {
+            System.out.println("Need one argument to work");
+            return;
+        }
+
         ManagedChannel channel = ManagedChannelBuilder
                 .forAddress("localhost", 50051)
                 .usePlaintext()
                 .build();
 
-        // do something
+        switch (args[0]) {
+            case "greet": doGreet(channel); break;
+            default: System.out.println("Keyword Invalid: " + args[0]);
+        }
 
         System.out.println("Shutting Down");
         channel.shutdown();
 
+    }
 
+    private static void doGreet(ManagedChannel channel) {
+        System.out.println("Enter doGreet");
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+        GreetingResponse response = stub.greet(GreetingRequest.newBuilder().setFirstName("john doe").build());
+
+        System.out.println("Greeting: " + response.getResult());
     }
 
 }
